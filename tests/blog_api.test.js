18edd0b9615e3862499f089cd209d9dadf963206
andregra <andregra717@gmail.com>
2022-11-03 16:06:07 +0100

    add solution 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,13 +7,13 @@ const Blog = require('../models/blog')
 const initialNotes = [
   {
     title: 'HTML is easy',
-    authot: 'Author one',
+    author: 'Author one',
     url: 'url/one',
     likes: 5
   },
   {
     title: 'HTML is very easy',
-    authot: 'Author two',
+    author: 'Author two',
     url: 'url/two',
     likes: 21
   }
@@ -25,12 +25,18 @@ beforeEach(async () => {
   await noteObject.save()
   noteObject = new Blog(initialNotes[1])
   await noteObject.save()
-})
+}, 100000)
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(initialNotes.length)
+}, 100000)
+
+test('unique identifier', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body.id).toBeDefined
 }, 100000)
 
 afterAll(() => {
