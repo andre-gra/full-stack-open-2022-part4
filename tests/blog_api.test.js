f9088d2ac2e231208f923f45d8e35484cf555d4b
andregra <andregra717@gmail.com>
2022-11-03 16:32:13 +0100

    add solution 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,7 +4,7 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialNotes = [
+const initialBlogs = [
   {
     title: 'HTML is easy',
     author: 'Author one',
@@ -21,16 +21,16 @@ const initialNotes = [
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let noteObject = new Blog(initialNotes[0])
+  let noteObject = new Blog(initialBlogs[0])
   await noteObject.save()
-  noteObject = new Blog(initialNotes[1])
+  noteObject = new Blog(initialBlogs[1])
   await noteObject.save()
 }, 100000)
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialNotes.length)
+  expect(response.body).toHaveLength(initialBlogs.length)
 }, 100000)
 
 test('unique identifier', async () => {
@@ -38,6 +38,30 @@ test('unique identifier', async () => {
 
   expect(response.body.id).toBeDefined
 }, 100000)
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'a very interesting post',
+    author: 'me',
+    url: 'url/url',
+    likes: 4 
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(contents).toContain(
+    'a very interesting post'
+  )
+})
 
 afterAll(() => {
   mongoose.connection.close()
