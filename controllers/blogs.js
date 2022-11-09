88100c02c86599fd5da995325865bc48e1c952a7
andregra <andregra717@gmail.com>
2022-11-09 17:22:15 +0100

    add solution 4.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
 
-  let decodedToken = ''
-  try {
-    decodedToken = jwt.verify(`${request.token}`, process.env.SECRET)
-  } catch (error) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   //check title and url
   if (body.title && body.author) {
@@ -44,23 +36,17 @@ blogsRouter.post('/', async (request, response) => {
 
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
-  let decodedToken = ''
-  try {
-    decodedToken = jwt.verify(`${request.token}`, process.env.SECRET)
-  } catch (error) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-    
-  console.log('decoded', decodedToken)
-
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response, next) => {
+  
   const blog = await Blog.findById(request.params.id)
+
+  const user = request.user
 
   if (!blog) {
     return response.status(400).json({error: 'This id doesn\'t exist'})
   }
 
-  if (blog.user.toString() !== decodedToken.id) {
+  if (blog.user.toString() !== user._id.toString()) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -34,9 +36,25 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
+const userExtractor = async (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7).trim()
+    let decodedToken = ''
+    try {
+      decodedToken = await jwt.verify(`${request.token}`, process.env.SECRET)
+      request.user = await User.findById(decodedToken.id)
+    } catch (error) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
