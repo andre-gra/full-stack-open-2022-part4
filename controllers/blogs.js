385ac907e768b9e90736a9db7852cde31444088f
andregra <andregra717@gmail.com>
2022-11-09 12:12:43 +0100

    add solution 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  // add user
-  const users = await User.find({})
-  const user = users[0]
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   //check title and url
   if (body.title && body.author) {
@@ -20,7 +32,7 @@ blogsRouter.post('/', async (request, response) => {
 
     const blog = new Blog({
       title: body.title,
-      author: body.author,
+      author: user.name,
       url: body.url,
       likes: body.likes,
       user: user._id
